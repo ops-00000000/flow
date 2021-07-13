27567b80e0090c1dd3e54e6aa2669a5df15aef27
ops-00000000 <belaevvova999@gmail.com>
2021-07-14 01:58:01 +0300

    Flow

---
+++ app/src/main/java/ru/netology/nmedia/dao/PostDao.kt
@@ -0,0 +1,40 @@
+package ru.netology.nmedia.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+import ru.netology.nmedia.entity.PostEntity
+import androidx.room.Update
+
+
+
+
+@Dao
+interface PostDao {
+    @Query("SELECT * FROM PostEntity WHERE visible  ORDER BY id DESC ")
+    fun getAll(): Flow<List<PostEntity>>
+
+    @Query("SELECT COUNT(*) == 0 FROM PostEntity")
+    suspend fun isEmpty(): Boolean
+
+    @Query("SELECT COUNT(*) FROM PostEntity")
+    suspend fun count(): Int
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(post: PostEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(posts: List<PostEntity>)
+
+    @Query("DELETE FROM PostEntity WHERE id = :id")
+    suspend fun removeById(id: Long)
+
+    @Query("UPDATE PostEntity SET visible =:visible WHERE id == :id")
+    suspend fun visible(visible:Boolean, id: Long)
+
+    @Query("UPDATE PostEntity SET visible =:visible")
+    suspend fun visibleALL(visible: Boolean)
+
+}
